93de2217bd187ba062b3c5303fd9da40b4e2801f
humlajonas <jonas@humladigital.no>
2023-09-27 19:12:04 +0200

    Fixed Career Benefits Text hover

--- src/careers.ts
+++ src/careers.ts
@@ -1,88 +1,86 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/mousewheel';
+import 'swiper/css/scrollbar';
+
 import gsap from 'gsap';
 import { SplitText } from 'gsap/all';
 import { initImageGalleryTabs } from 'src/modules/imageGalleryTabs';
 import Swiper from 'swiper';
+import { Mousewheel, Navigation, Scrollbar } from 'swiper/modules';
+import { type SwiperOptions } from 'swiper/types/index.d';
 
 import { hideEmptyDepartments } from '$utils/hideEmptyDepartments';
+
+import { initDiscoverGallerySlider } from './modules/discoverPostSlider';
 gsap.registerPlugin(SplitText);
-export {};
-declare global {
-  interface Window {
-    WebflowEditor: unknown;
-  }
-}
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  if (!window.WebflowEditor) {
-    console.log('Wont run in Webflow editor');
-  } else {
-    console.log('Will run in the Webflow editor');
-  }
-});
+
 initImageGalleryTabs();
 hideEmptyDepartments();
+initDiscoverGallerySlider();
 
-/*CAREERS IMAGE GALLERY SLIDER */
-$('.section_careers-image-slider').each(function (index) {
-  console.log($(this).find('.swiper'));
-  const careersImageSlider = new Swiper($(this).find('.swiper')[0], {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    freeMode: true,
-    slidesPerGroup: 1,
-    spaceBetween: 20,
-    mousewheel: {
-      forceToAxis: true,
+const careersImageSliderParams: SwiperOptions = {
+  modules: [Navigation, Mousewheel, Scrollbar],
+  direction: 'horizontal',
+  slidesPerView: 1,
+  freeMode: true,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  mousewheel: {
+    forceToAxis: true,
+  },
+  speed: 300,
+  // Responsive breakpoints
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+      spaceBetween: 9.6,
     },
-    speed: 300,
-    // Responsive breakpoints
-    breakpoints: {
-      320: {
-        slidesPerView: 'auto',
-        spaceBetween: 9.6,
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 'auto',
-        spaceBetween: 9.6,
-      },
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 2,
-      },
-      // when window width is >= 992px
-      992: {
-        slidesPerView: 3,
-      },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 'auto',
+      spaceBetween: 9.6,
     },
-    navigation: {
-      nextEl: $(this).find('.swiper-arrow.button-next')[0],
-      prevEl: $(this).find('.swiper-arrow.button-prev')[0],
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
     },
-    scrollbar: {
-      el: $(this).find('.swiper-scrollbar')[0],
-      dragSize: 90,
-      draggable: true,
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
     },
-  });
-});
-/*
+  },
+  navigation: {
+    nextEl: '#careers-images-next',
+    prevEl: '#careers-images-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    dragSize: 90,
+    draggable: true,
+  },
+};
+const careersImageSlider = new Swiper('.image-gallery', careersImageSliderParams);
+
 if (window.innerWidth > 1399) {
-  $('.wrapper_career-benefit').each(function (i, element) {
+  $('.wrapper_career-benefit').each(function (_i, _element) {
     const splitTextTimeline = gsap.timeline({ paused: true, reversed: true }),
-      theText = $(this).find('.is_career-benefit-typed'),
-      mySplitText = new SplitText(theText, { type: 'words,chars' }),
-      { chars } = mySplitText;
+      careerBenefit = $(this).find('.is_career-benefit-typed'),
+      splitText = new SplitText(careerBenefit, { type: 'words,chars' }),
+      { chars } = splitText;
 
-    splitTextTimeline.to(chars, 0.01, { autoAlpha: 0 }, 0.01);
+    splitTextTimeline.from(chars, {
+      autoAlpha: 0,
+      duration: 0.01,
+      stagger: 0.01,
+    });
 
-    $(this).hover(makeItWork, makeItWork);
+    $(this).on('mouseenter', makeItWork).on('mouseleave', makeItWork);
 
     function makeItWork() {
       splitTextTimeline.reversed() ? splitTextTimeline.play() : splitTextTimeline.reverse();
     }
   });
 }
-*/
