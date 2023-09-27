7128d8654001fce763eea65ca39d13dd120bd799
humlajonas <jonas@humladigital.no>
2023-09-27 08:15:18 +0200

    dist public

---
+++ dist/eve.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/eve.ts
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    if (!window.WebflowEditor) {
+      console.log("Wont run in Webflow editor");
+    } else {
+      console.log("Will run in the Webflow editor");
+    }
+  });
+})();
+//# sourceMappingURL=eve.js.map
