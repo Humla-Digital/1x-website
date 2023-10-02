3903fdd96764ef1c68c819ceef46eb016f9b7408
humlajonas <jonas@humladigital.no>
2023-10-02 12:39:59 +0200

    updates

--- src/neo.ts
+++ src/neo.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import SplitText from 'gsap/SplitText';
 import $ from 'jquery';
 
 import { faqModule } from './modules/faqs';
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 declare global {
   interface Window {
     WebflowEditor: unknown;
@@ -71,6 +73,82 @@ function neoScrollScene() {
     3
   );
 }
+
+function toggleSpecs() {
+  const imperialHeight = '5 foot 6 inches';
+  const imperialWeight = '66 pounds';
+  const imperialWalkspeed = '2.5 miles/hour walk speed';
+  const imperialRunspeed = '7.5 miles/hour run speed';
+  const imperialCarry = '44 pound carry capacity';
+
+  const metricHeight = '1.73 meters';
+  const metricWeight = '30 kilograms';
+  const metricWalkspeed = '4 kilometers/hour walk speed';
+  const metricRunspeed = '12 kilometers/hour run speed';
+  const metricCarry = '20 kilograms carry capacity';
+
+  $('.metric-toggle_item').on('click', function () {
+    $(this).addClass('is-active');
+    $(this).siblings('.metric-toggle_item').removeClass('is-active');
+  });
+  $('#imperial').on('click', function () {
+    $('#height').text(imperialHeight);
+    $('#weight').text(imperialWeight);
+    $('#walk-speed').text(imperialWalkspeed);
+    $('#run-speed').text(imperialRunspeed);
+    $('#carry-capacity').text(imperialCarry);
+  });
+  $('#metric').on('click', function () {
+    $('#height').text(metricHeight);
+    $('#weight').text(metricWeight);
+    $('#walk-speed').text(metricWalkspeed);
+    $('#run-speed').text(metricRunspeed);
+    $('#carry-capacity').text(metricCarry);
+  });
+}
+toggleSpecs();
+
+function androidSpecs() {
+  if (window.innerWidth > 1399) {
+    $('.wrapper_splittext-row').each(function (_i, _element) {
+      const splitTextTimeline = gsap.timeline({ paused: true, reversed: true }),
+        careerBenefit = $(this).find('.is-splittext'),
+        splitText = new SplitText(careerBenefit, { type: 'words,chars' }),
+        { chars } = splitText;
+
+      splitTextTimeline.from(chars, {
+        autoAlpha: 0,
+        duration: 0.01,
+        stagger: 0.01,
+      });
+      $(this).on('mouseenter', typeText).on('mouseleave', typeText);
+      function typeText() {
+        splitTextTimeline.reversed() ? splitTextTimeline.play() : splitTextTimeline.reverse();
+      }
+    });
+  } else {
+    $('.wrapper_splittext-row').on('click', function () {
+      $(this).toggleClass('is-active');
+      if ($(this).hasClass('is-active')) {
+        $(this).find('.is-splittext').addClass('is-active');
+        const splitTextTimeline = gsap.timeline({ paused: true, reversed: true }),
+          careerBenefit = $(this).find('.is-splittext'),
+          splitText = new SplitText(careerBenefit, { type: 'words,chars' }),
+          { chars } = splitText;
+        splitTextTimeline.from(chars, {
+          autoAlpha: 0,
+          duration: 0.01,
+          stagger: 0.01,
+        });
+        splitTextTimeline.reversed() ? splitTextTimeline.play() : splitTextTimeline.reverse();
+      } else {
+        $(this).removeClass('is-active');
+        $(this).find('.is-splittext').removeClass('is-active');
+      }
+    });
+  }
+}
+androidSpecs();
 
 $('.android_value-props-tab-link').on('click', function () {
   $(this).find('.android_value-props-content').addClass('is-active');
