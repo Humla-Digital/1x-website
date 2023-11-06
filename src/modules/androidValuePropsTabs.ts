25e65f27cd55ee0d1243be612144134d963aec75
Jonas Linnestad <137791671+humlajonas@users.noreply.github.com>
2023-11-06 18:33:45 +0100

    Updated androids value props tabs

--- src/modules/androidValuePropsTabs.ts
+++ src/modules/androidValuePropsTabs.ts
@@ -1,5 +1,9 @@
 /* eslint-disable no-return-assign */
 /* eslint-disable prefer-rest-params */
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export function androidValuePropsTabs() {
   // Fix for Safari
@@ -20,32 +24,114 @@ export function androidValuePropsTabs() {
     );
   }
 
-  // Start Tabs
-  let tabTimeout: number | undefined;
-  clearTimeout(tabTimeout);
-  tabLoop();
+  const tabTimelines: Array<gsap.core.Timeline> = [];
 
-  // Connect your class names to elements
-  function tabLoop() {
-    tabTimeout = setTimeout(function () {
-      const $next = $('.android_value-props-menu').children('.w--current:first').next();
+  $('.android_value-props-tab-link').each(function () {
+    const $progressBar = $(this).find('.android_value-props-progbar');
 
-      if ($next.length) {
-        $next.trigger('click'); // user click resets timeout
-      } else {
-        $('.android_value-props-tab-link:first').trigger('click');
-      }
-    }, 10000); // 10 Second Rotation
+    const tabTimeline = gsap.timeline({ paused: true });
+    tabTimeline.to($progressBar, {
+      width: '100%',
+      duration: 15,
+      ease: 'none',
+      onComplete: function () {
+        gsap.set($progressBar, { width: '0%' });
+      },
+    });
+
+    tabTimelines.push(tabTimeline);
+  });
+
+  const tabAutoplay = gsap.delayedCall(15, function () {
+    nextTab();
+  });
+  tabAutoplay.pause();
+  ScrollTrigger.create({
+    trigger: '.android_value-props-tabs',
+    onEnter: () => tabAutoplay.play(),
+    onLeave: () => tabAutoplay.pause(),
+    onEnterBack: () => tabAutoplay.play(),
+    onLeaveBack: () => tabAutoplay.pause(),
+  });
+
+  // Create a ScrollTrigger for the .image-tabs element
+  ScrollTrigger.create({
+    trigger: '.android_value-props-tabs',
+    onEnter: () => {
+      // Play the current tab's progress bar animation
+      const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+      const currentIndex = $currentTab.index();
+      tabTimelines[currentIndex].play();
+    },
+    onLeave: () => {
+      // Pause the current tab's progress bar animation
+      const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+      const currentIndex = $currentTab.index();
+      tabTimelines[currentIndex].pause();
+    },
+    onEnterBack: () => {
+      // Play the current tab's progress bar animation when scrolling back into view
+      const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+      const currentIndex = $currentTab.index();
+      tabTimelines[currentIndex].play();
+    },
+    onLeaveBack: () => {
+      // Pause the current tab's progress bar animation when scrolling back out of view
+      const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+      const currentIndex = $currentTab.index();
+      tabTimelines[currentIndex].pause();
+    },
+  });
+
+  // Pause the initial progress bar animation
+  tabTimelines[0].pause();
+
+  // Play the progress bar of the first tab on load
+  //tabTimelines[0].play();
+
+  function nextTab() {
+    const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+    const currentIndex = $currentTab.index();
+
+    tabTimelines[currentIndex].pause();
+
+    const $next = $currentTab.next();
+    if ($next.length) {
+      $next.trigger('click');
+    } else {
+      $('.android_value-props-tab-link:first').trigger('click');
+    }
+
+    const $newCurrentTab = $('.android_value-props-menu').children('.w--current:first');
+    const newIndex = $newCurrentTab.index();
+    tabTimelines[newIndex].play();
+
+    tabAutoplay.restart(true);
   }
 
-  // Reset Loops
+  $('.android_value-props-menu')
+    .on('mouseover', function () {
+      tabAutoplay.pause();
+      tabTimelines.forEach((timeline) => timeline.pause());
+    })
+    .on('mouseleave', function () {
+      tabAutoplay.resume();
+      const $currentTab = $('.android_value-props-menu').children('.w--current:first');
+      const currentIndex = $currentTab.index();
+      tabTimelines[currentIndex].play();
+    });
+
   $('.android_value-props-tab-link').on('click', function () {
     $(this)
       .siblings('.android_value-props-tab-link')
       .find('.accordion_status')
       .removeClass('active');
     $(this).find('.accordion_status').addClass('active');
-    clearTimeout(tabTimeout);
-    tabLoop();
+    tabTimelines.forEach((timeline) => timeline.progress(0).pause());
+    const $clickedTab = $(this);
+    const index = $clickedTab.index();
+
+    tabTimelines[index].restart();
+    tabAutoplay.restart(true);
   });
 }
