63a4de4dd1be9013f1936de83449c8e7beb1ec00
Jonas Linnestad <137791671+humlajonas@users.noreply.github.com>
2024-01-23 11:49:36 +0100

    Update hideEmptyDynLists.ts

--- src/utils/hideEmptyDynLists.ts
+++ src/utils/hideEmptyDynLists.ts
@@ -1,6 +1,6 @@
 export function hideEmptyDynSections() {
   $('.w-dyn-empty')
-    .not('pencil_banner-cms-empty')
+    .not('.pencil_banner-cms-empty')
     .parents('section')
     .each(function () {
       $(this).remove();
