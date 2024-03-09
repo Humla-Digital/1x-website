e9c4c83df0013355d14e489ff914b8adb6fe12cf
Jonas Linnestad <137791671+humlajonas@users.noreply.github.com>
2024-03-09 16:37:37 +0100

    added menu click scroll disabling

--- src/global.ts
+++ src/global.ts
@@ -17,6 +17,13 @@ if ($('.menu-link.w--current')) {
   $('.menu-link.w--current').siblings('.menu-link').css('opacity', '0.5');
 }
 
+$(".menu-open").on("click", function() {
+  $("body").addClass("overflow-hidden")
+})
+$(".menu-close").on("click", function() {
+  $("body").removeClass("overflow-hidden")
+})
+
 declare global {
   interface Window {
     WebflowEditor: unknown;
@@ -53,7 +60,7 @@ function navbarAnim() {
 }
 
 function footerAnim() {
-  $('footer').each(function () {
+  $('footer').each(function() {
     const triggerElement = $('.footer_circles-wrapper');
     const target = $('.footer_circles-wrapper').find('path');
     const footerAnimation = gsap.timeline({
@@ -74,21 +81,21 @@ function footerAnim() {
 
 /**SIDEBAR MENU HOVER STATES*/
 if (window.innerWidth > 992) {
-  $('.sidebar-menu-link-row').on('mouseover', function () {
+  $('.sidebar-menu-link-row').on('mouseover', function() {
     $(this).siblings('.sidebar-menu-link-row').css('opacity', '0.3');
   });
-  $('.sidebar-menu-link-row').on('mouseleave', function () {
+  $('.sidebar-menu-link-row').on('mouseleave', function() {
     $(this).siblings('.sidebar-menu-link-row').css('opacity', '1');
   });
   /* SUB SIDEBAR MENU HOVER STATES */
-  $('.sidebar-sub-menu-link-row').on('mouseover', function () {
+  $('.sidebar-sub-menu-link-row').on('mouseover', function() {
     $(this).siblings('.sidebar-sub-menu-link-row').css('opacity', '0.3');
   });
-  $('.sidebar-sub-menu-link-row').on('mouseleave', function () {
+  $('.sidebar-sub-menu-link-row').on('mouseleave', function() {
     $(this).siblings('.sidebar-sub-menu-link-row').css('opacity', '1');
   });
   /* SUB SIDEBAR HOVER SET FIRST LEVEL LINKS */
-  $('.sidebar-menu-drawer').on('mouseover', function () {
+  $('.sidebar-menu-drawer').on('mouseover', function() {
     if ($('#menu-content-androids').hasClass('is-active')) {
       $('.sidebar-menu-link-row:not(#menu-item-androids)').css('opacity', '0.3');
     } else if ($('#menu-content-careers').hasClass('is-active')) {
@@ -98,7 +105,7 @@ if (window.innerWidth > 992) {
     }
   });
   /* HIDE AND SHOW SUB SIDEBAR MENU CONTENTS */
-  $('#menu-item-androids').on('mouseover', function () {
+  $('#menu-item-androids').on('mouseover', function() {
     if (!$('.sidebar-menu-drawer').hasClass('is-active')) {
       $('.sidebar-menu-drawer').addClass('is-active');
     }
@@ -106,7 +113,7 @@ if (window.innerWidth > 992) {
     $('#menu-content-careers').removeClass('is-active');
     //$('#menu-content-discover').removeClass('is-active');
   });
-  $('#menu-item-careers').on('mouseover', function () {
+  $('#menu-item-careers').on('mouseover', function() {
     if (!$('.sidebar-menu-drawer').hasClass('is-active')) {
       $('.sidebar-menu-drawer').addClass('is-active');
     }
@@ -114,13 +121,13 @@ if (window.innerWidth > 992) {
     $('#menu-content-androids').removeClass('is-active');
     $('#menu-content-discover').removeClass('is-active');
   });
-  $('#menu-item-home').on('mouseover', function () {
+  $('#menu-item-home').on('mouseover', function() {
     $('.sidebar-menu-drawer').removeClass('is-active');
   });
-  $('#menu-item-discover').on('mouseover', function () {
+  $('#menu-item-discover').on('mouseover', function() {
     $('.sidebar-menu-drawer').removeClass('is-active');
   });
-  $('#menu-item-about').on('mouseover', function () {
+  $('#menu-item-about').on('mouseover', function() {
     $('.sidebar-menu-drawer').removeClass('is-active');
   });
   /*
@@ -133,7 +140,7 @@ if (window.innerWidth > 992) {
     $('#menu-content-careers').removeClass('is-active');
   });*/
 } else {
-  $('#mobile-menu-item-androids').on('click', function () {
+  $('#mobile-menu-item-androids').on('click', function() {
     if (!$('.sidebar-menu-drawer').hasClass('is-active')) {
       $('.sidebar-menu-drawer').addClass('is-active');
     }
@@ -141,7 +148,7 @@ if (window.innerWidth > 992) {
     $('#menu-content-careers').removeClass('is-active');
     $('#menu-content-discover').removeClass('is-active');
   });
-  $('#mobile-menu-item-careers').on('click', function () {
+  $('#mobile-menu-item-careers').on('click', function() {
     if (!$('.sidebar-menu-drawer').hasClass('is-active')) {
       $('.sidebar-menu-drawer').addClass('is-active');
     }
@@ -149,7 +156,7 @@ if (window.innerWidth > 992) {
     $('#menu-content-androids').removeClass('is-active');
     $('#menu-content-discover').removeClass('is-active');
   });
-  $('#menu-item-discover').on('click', function () {
+  $('#menu-item-discover').on('click', function() {
     if (!$('.sidebar-menu-drawer').hasClass('is-active')) {
       $('.sidebar-menu-drawer').addClass('is-active');
     }
@@ -160,20 +167,20 @@ if (window.innerWidth > 992) {
 }
 
 /*CLOSE SIDEBAR BUTTON AND OVERLAY TARGET*/
-$('.sidebar-close').on('click', function () {
+$('.sidebar-close').on('click', function() {
   $('.sidebar-menu-drawer').removeClass('is-active');
   $('.menu-drawer-content').removeClass('is-active');
   $('.sidebar-menu-link-row').css('opacity', '1');
 });
 
 /**UNDER 992px SUB SIDEBAR BACK BUTTON */
-$('.tablet_sidebar-back').on('click', function () {
+$('.tablet_sidebar-back').on('click', function() {
   $('.sidebar-menu-drawer').removeClass('is-active');
 });
 
 function featuredPostAnim() {
   if ('.section_announcement-featured-post') {
-    $('.section_announcement-featured-post').each(function () {
+    $('.section_announcement-featured-post').each(function() {
       const triggerElement = $(this);
       const targets = gsap.utils.toArray([$(this).find('div'), $(this).find('p')]);
       const targetImg = $(this).find('img');
