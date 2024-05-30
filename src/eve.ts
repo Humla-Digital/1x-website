a2f00e6afafe16a3486bac860a9d46cec7736569
Jonas Linnestad <137791671+humlajonas@users.noreply.github.com>
2024-05-30 19:27:49 +0200

    Update eve.ts

--- src/eve.ts
+++ src/eve.ts
@@ -2,13 +2,14 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/mousewheel';
+import 'swiper/css/controller';
 
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SplitText } from 'gsap/SplitText';
 import Swiper from 'swiper';
-import { Controller, Mousewheel, Navigation, Pagination } from 'swiper/modules';
-import { type SwiperOptions } from 'swiper/types';
+import { Controller, Mousewheel, Navigation } from 'swiper/modules';
+import { type SwiperOptions } from 'swiper/types/index.d';
 
 import { pauseVideo } from '$utils/pauseVideo';
 
